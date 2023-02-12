2b7875d68bb19172641bd9b605a43ad85842b255
sharda.borkar <borkar.sharda92@gmail.com>
2023-02-12 19:02:09 +0530

    latest code

--- src/test/java/Tutorial/SeleniumMaven/GitSample.java
+++ src/test/java/Tutorial/SeleniumMaven/GitSample.java
@@ -6,6 +6,7 @@ public class GitSample {
 		System.out.println("First git code");
 		System.out.println("change 1 code");
 		System.out.println("change 2 code");
+		System.out.println("change 3 code");
 
 	}
 
