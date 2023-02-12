a6be4c8ffb342e6c34d51e0a871fb9616ea85e15
sharda.borkar <borkar.sharda92@gmail.com>
2023-02-12 17:03:50 +0530

    Updated code

--- src/test/java/Tutorial/SeleniumMaven/GitSample.java
+++ src/test/java/Tutorial/SeleniumMaven/GitSample.java
@@ -4,6 +4,8 @@ public class GitSample {
 
 	public static void main(String[] args) {
 		System.out.println("First git code");
+		System.out.println("change 1 code");
+		System.out.println("change 2 code");
 
 	}
 
